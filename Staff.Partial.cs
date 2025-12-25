1b5f13ab1f2423668db701911c6405c47252bec0
Katarina Holm <katarinaholm89@gmail.com>
2025-12-25 16:10:59 +0100

    Added partial classes cause I want to keep constructors but need to do a new scaffold to update my classes from database-

---
+++ Staff.Partial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Gymnasieskola.Models
+{
+
+    public partial class Staff
+    {
+        public Staff() { }
+        public Staff(string p, string fN, string lN, string sSN, string hA, string pN, string e)
+        {
+            Profession = p;
+            FirstName = fN;
+            LastName = lN;
+            SocialSecurityNr = sSN;
+            HomeAddress = hA;
+            PhoneNr = pN;
+            Email = e;
+        }
+    }
+}
+//scaffold-DbContext “Data Source = KAPTOP; Database=school_db Integrated Security = True; Trust Server Certificate=True;” Microsoft.EntityFrameworkCore.SqlServer 
+//- OutputDir Models -ContextDir Data -Context School_dbContext -Force
---
+++ Student.Partial.cs
@@ -0,0 +1,22 @@
+﻿using Gymnasieskola.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gymnasieskola.Models
+{
+    public partial class Student
+    {
+        public Student() { }
+        public Student(string fN, string lN, string sSN, string hA, string pN, string e, int? cI)
+        {
+            FirstName = fN;
+            LastName = lN;
+            SocialSecurityNr = sSN;
+            HomeAddress = hA;
+            PhoneNr = pN;
+            Email = e;
+            ClassId = cI;
+        }
+    }
+}
